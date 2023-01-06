ca5b9722bb9eeb2578b2b426e849d6cbe4e433cf
mahaElmorsy <mahaosmanelmorsy@gmail.com>
2023-01-06 18:15:29 +0200

    models

--- boards/models.py
+++ boards/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Board(models.Model):
+    name=models.CharField(max_length=50,unique=True)
+    description=models.CharField(max_length=150)
+
+class Topic(models.Model):
+    subject=models.CharField(max_length=250)
+    board=models.ForeignKey(Board,related_name='topics',on_delete=models.CASCADE)
+    created_by=models.ForeignKey(User,related_name='topics',on_delete=models.CASCADE)
+
