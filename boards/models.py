8aa6287e71b1817aa3f89b031594eb7bffd8deea
mahaElmorsy <mahaosmanelmorsy@gmail.com>
2023-01-11 14:03:59 +0200

    update

--- boards/models.py
+++ boards/models.py
@@ -1,13 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class Board(models.Model):
-    name=models.CharField(max_length=50,unique=True)
-    description=models.CharField(max_length=150)
+    name = models.CharField(max_length=50, unique=True)
+    description = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
 
 class Topic(models.Model):
-    subject=models.CharField(max_length=250)
-    board=models.ForeignKey(Board,related_name='topics',on_delete=models.CASCADE)
-    created_by=models.ForeignKey(User,related_name='topics',on_delete=models.CASCADE)
+    subject = models.CharField(max_length=250)
+    board = models.ForeignKey(Board, related_name='topics', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, related_name='topics', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
 
+
+class Post(models.Model):
+    message = models.CharField(max_length=3000)
+    topic = models.ForeignKey(Topic, related_name='posts', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
--- boards/views.py
+++ boards/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse,Http404
+from .models import Board
+
+
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse('hello maha')
+    boards = Board.objects.all()
+    return render(request, 'home.html', {'boards': boards})
+
+
+def about(request):
+    return render(request, 'about.html')
+def board_topic(request,board_id):
+    # try:
+    #     board = Board.objects.get(pk=board_id)
+    #     print(board)
+    # except Board.DoesNotExist:
+    #     raise Http404
+    board=get_object_or_404(Board, pk=board_id)
+    return render(request, 'topics.html', {'board':board})
